078e949281e3034612e3988347fd57d527b76b1b
m-subhan-tahir <subhantahir430@gmail.com>
2025-12-09 20:31:27 +0500

    build-successfully

--- components/main/CartMain.tsx
+++ components/main/CartMain.tsx
@@ -137,7 +137,7 @@ export default function CartMain() {
   }
 
   return (
-    <Container className="max-w-[80rem] py-12">
+    <Container className="max-w-4xl py-12">
       <h2 className="text-3xl font-bold mb-8 text-center">Your Cart</h2>
 
       <div className="grid grid-cols-1 md:grid-cols-3 gap-10">
